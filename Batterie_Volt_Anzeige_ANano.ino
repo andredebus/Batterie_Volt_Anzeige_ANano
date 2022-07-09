6e130c7a8e3d722ff6426099ee8ed19097995383
andredebus <93975866+andredebus@users.noreply.github.com>
2022-07-09 14:54:25 +0200

    Update Batterie_Volt_Anzeige_ANano.ino

--- Batterie_Volt_Anzeige_ANano.ino
+++ Batterie_Volt_Anzeige_ANano.ino
@@ -13,37 +13,37 @@ String volt_s;
 
 String voltBerechnet(int v_in)
 {
-  //Alle Konstanten als double definiert damit beim Rechnen int nicht mit float kollidiert
-  const double BEREICH    = 1024.0;         //Der Bereich von 0 - 1023 sind 1024 Werte
-  const double R1         = 9950.0;         //gemessener Wiederstandswert von R1
-  const double R2         = 998.0;          //gemessener Wiederstandswert von R2
-  const double V_MAX      = 5.0;            //maximale Spannung die an einem Analog Pin anliegen darf "wenn größer dann bruzzel..." 
-  const double AADC       = 0.5;            //korrektur des Arduino ADC Fehlers kuckst du hier https://skillbank.co.uk/arduino/adc.htm für eine ausführliche Erklärung
+  //Alle Konstanten als double definiert damit beim Rechnen int nicht mit float kollidiert.
+  const double BEREICH    = 1024.0;                //Der Bereich von 0 - 1023 sind 1024 Werte.
+  const double R1         = 9950.0;                //Gemessener Wiederstandswert von R1.
+  const double R2         = 998.0;                 //Gemessener Wiederstandswert von R2.
+  const double V_MAX      = 5.0;                   //Maximale Spannung die an einem Arduino Nano Analog Pin anliegen darf. 
+  const double AADC       = 0.5;                   //Korrektur des Arduino ADC Fehlers https://skillbank.co.uk/arduino/adc.htm für eine ausführliche Erklärung.
     
   
-  float volt_in = (v_in * V_MAX + AADC) / BEREICH; // Umrechnen von dem Bereichswert in die Spannung die an A0 anliegt
-  float volt = volt_in / (  R2/(R1+R2)  );         // Die Spannung von A0 auf die tatsächlich Spannung umrechnen mit der Formel für Spannungsteiler   
+  float volt_in = (v_in * V_MAX + AADC) / BEREICH; // Umrechnen von dem Bereichswert in die Spannung die an A0 anliegt.
+  float volt = volt_in / (  R2/(R1+R2)  );         // Die Spannung von A0 auf die tatsächlich Spannung umrechnen mit der Formel für Spannungsteiler.   
   
   
-  if (volt < 0.01) volt = 0 ;                      // durch die Korrektur des ADC Fehlers in der Formel wird der Wert immer >0 sein auch bei 0 Spannung
-                                                   // deshalb die Korrektur der Korrektur... klingt blöd is aber so... weil (0 x 5.0 + 0.5/ 1024 = 0,00048828125) 
-  if (volt < 10)                                   // wenn die berechnete Spannung kleiner 10 Volt ist  
+  if (volt < 0.01) volt = 0 ;                      // Durch die Korrektur des ADC Fehlers in der Formel wird der Wert immer >0 sein auch bei 0 Spannung
+                                                   // deshalb < 0,01 = 0 weil (0 x 5.0 + 0.5/ 1024 = 0,00048828125). 
+  if (volt < 10)                                   // Wenn die berechnete Spannung kleiner 10 Volt ist.  
     
     { 
     
-    String volt_s = "0" + String(volt) + "V";    // hätte ich gerne als Präfix eine "0" und als Suffix ein "V"
-    volt_s.replace(".",",");                      // wir sind in Deutschland Dezimaltrennzeichen ist ein "," kein "." 
-    return volt_s;                                // und als Stringvariable volt_s zurückgeben
+    String volt_s = "0" + String(volt) + "V";     // Als Präfix eine "0" und als Suffix ein "V" setzen.
+    volt_s.replace(".",",");                      // Deutsches Dezimaltrennzeichen setzen, 
+    return volt_s;                                // und als Stringvariable volt_s zurückgeben.
     
     }
   
-  else                                            // wenn die berechnete Spannung größer oder gleich 10 Volt ist (sonst wär sie ja kleiner) 
+  else                                            // Wenn die berechnete Spannung größer oder gleich 10 Volt ist. 
       
       {
       
-      String volt_s = String(volt) + "V";        // hätte ich gerne als Suffix ein "V"
-      volt_s.replace(".",",");                    // wir sind in Deutschland Dezimaltrennzeichen ist ein "," kein "." 
-      return volt_s;                              // und als Stringvariable volt_s zurückgeben
+      String volt_s = String(volt) + "V";         // Als Suffix ein "V".
+      volt_s.replace(".",",");                    // Deutsches Dezimaltrennzeichen setzen, 
+      return volt_s;                              // und als Stringvariable volt_s zurückgeben.
       
       }
 }
